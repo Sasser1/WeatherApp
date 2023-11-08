633b9b1466e2e9ba6e9c64448e200074d63886bc
Sasser1 <davidbozovic1@gmail.com>
2023-11-08 20:07:14 +0100

    Added layout

--- PMAProjekat/app/build.gradle.kts
+++ PMAProjekat/app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
     defaultConfig {
         applicationId = "com.example.vremenskaprognoza"
         minSdk = 24
-        targetSdk = 33
+            targetSdk = 33
         versionCode = 1
         versionName = "1.0"
 
@@ -51,7 +51,7 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.activity:activity-compose:1.8.0")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
@@ -66,4 +66,21 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+
+    // Use AndroidX for RecyclerView
+    implementation("androidx.recyclerview:recyclerview:1.3.2")
+    // For control over item selection of both touch and mouse driven selection
+    implementation("androidx.recyclerview:recyclerview-selection:1.1.0")
+
+    implementation ("androidx.constraintlayout:constraintlayout:2.1.4") // Use the latest version available
+
+
+    // Retrofit for making API requests
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+
+    // Gson for JSON parsing
+    implementation("com.google.code.gson:gson:2.8.9")
+
+    implementation("com.google.android.gms:play-services-location:21.0.1")
 }
